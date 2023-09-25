0f376f9e0b8a82eb6c9e6131ac0e150401f8765b
PavelchukMax <120489328+PavelchukMax@users.noreply.github.com>
2023-09-25 18:20:30 +0300

    Delete lab2/src directory

--- lab2/src/components/footer.js
+++
@@ -1,7 +0,0 @@
-function Footer(){
-    return(
-<footer className="App-footer">
-<h3>This is footer</h3>
-</footer>)
-    }
-    export default Footer
--- lab2/src/components/welcome.js
+++
@@ -1,4 +0,0 @@
-function Welcome(prop){
-    return( <h2> Hello, {prop.name} </h2>)
-}
-export default Welcome
