ebef04208714e85666082de5c09cb278f220dd91
PavelchukMax <120489328+PavelchukMax@users.noreply.github.com>
2023-09-25 18:19:48 +0300

    Add files via upload

--- lab2/src/App.js
+++ lab2/src/App.js
@@ -3,10 +3,10 @@ import './App.css';
 import Header from './components/head.js'
 import Body from './components/body.js'
 import Footer from './components/footer.js'
-const tovars =[{id:1, value:'Iphone15'}, {id: 2, value:'Iphone14'},{id:3, value:'Airpods'},{id: 4, value: 'Samsung S21'}, { id: 5, value:'Pixel 7 pro'}]
+const tovars =['Iphone15','Iphone14', 'Airpods','Samsung S21', 'Pixel 7 pro']
 
 function App() {
-  
+ 
   return (
     <div className ="App">
       <img src={logo} className="App-logo" alt="logo" />
--- lab2/src/components/body.js
+++ lab2/src/components/body.js
@@ -1,14 +1,38 @@
-function ChecksList(prop){
-  return <input class="checkbox" type="checkbox" value={prop.value}/>
-}
+import React, { useState } from "react";
 function Body(prop)
 {
-  const tovars = prop.tovar;
-  const checksList = tovars.map((tovar) =>
-    <ChecksList id={tovar.id} value={tovar.value} />);
+  const [checked, setChecked] = useState([]);
+
+  const checkList =prop.tovar;
+
+  const handleCheck = (event) => {
+    var updatedList = [...checked];
+    if (event.target.checked) {
+      updatedList = [...checked, event.target.value];
+    } else {
+      updatedList.splice(checked.indexOf(event.target.value), 1);
+    }
+    setChecked(updatedList);
+  };
+
+  const checkedItems = checked.length ? checked.reduce((total, item) => {
+        return total + ", " + item;
+      }) : "";
+  
+      var isChecked = (item) =>
+  checked.includes(item) ? "checked-item" : "not-checked-item";
+ 
   return( 
   <body className="App-body"> 
-  {checksList}  
+ 
+ {checkList.map((item, index) => (
+            <div key={index}>
+              <input value={item} type="checkbox" onChange={handleCheck} />
+              <span className={isChecked(item)}>{item}</span>
+            </div>
+          ))}
+        {`You checked: ${checkedItems}`}
+ 
   </body>
   )
 }
